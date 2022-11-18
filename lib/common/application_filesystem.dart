e3fe0e2824bda813024b3f971cfc9432ead288fc
F <fgadea>
2022-11-18 21:10:14 +0100

    wip

---
+++ lib/common/application_filesystem.dart
@@ -0,0 +1,6 @@
+import '../models/app_directory.dart';
+
+AppDirectory getDirectory() {
+  return AppDirectory(
+      path: "usr/fgadea", items: ['Documents', 'Desktop', 'Downloads']);
+}
---
+++ lib/models/app_directory.dart
@@ -0,0 +1,6 @@
+class AppDirectory {
+  String path;
+  List<String> items;
+
+  AppDirectory({required this.path, required this.items});
+}
